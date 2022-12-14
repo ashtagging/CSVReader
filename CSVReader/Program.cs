2c2c25393f221a427533f16bd120e927349d56b1
Ashley Littlewood <alittlewood@stayinfront.com>
2022-12-14 10:48:54 +0000

    Added code for reading directories

--- CSVReader/Program.cs
+++ CSVReader/Program.cs
@@ -1,11 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CsvHelper;
+using System.Globalization;
 
-static void Testing()
+string rootPath = @"C:\Users\alittlewood\Documents\FileSystem";
+
+//public static string[] GetDirectories(string path, string searchPattern, System.IO.SearchOption searchOption);
+
+//Prints to the Console the file path of each folder within the specified folder
+//string[] directories = Directory.GetDirectories(rootPath);
+
+//foreach (string d in directories)
+//{
+//    Console.WriteLine(d);
+//}
+
+//Prints to the Console the File paths of all fodlers within the file path, not just top directories
+string[] directories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+
+foreach (string d in directories)
 {
-    Console.WriteLine("Testing123");
+    Console.WriteLine(d);
 }
 
-Testing();
 
-public class 
+

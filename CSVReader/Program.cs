e94c4237714033ef80b2cda37497500ad627ab6d
Ashley Littlewood <alittlewood@stayinfront.com>
2022-12-14 09:55:45 +0000

    Add project files.

---
+++ CSVReader/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static void Testing()
+{
+    Console.WriteLine("Testing123");
+}
+
+Testing();
+
+public class 

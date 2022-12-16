5ac60e7cde6eba5d3e71ffc28a683d496ce191d9
Ashley Littlewood <alittlewood@stayinfront.com>
2022-12-16 13:14:19 +0000

    Testing Properties

--- CSVReader/Program.cs
+++ CSVReader/Program.cs
@@ -16,6 +16,9 @@ using (var streamReader = new StreamReader(csvPath))
 
 Console.ReadLine();
 
+// Need to add line to change blanks to 0 so that it can be added to the correct data type,
+// or make string and then add fucntionality for if string "" => change to string "0" then ToINT
+
 /// <summary>
 ///  change all to strings and see which ones are a problem (get it working with strings first) (dynamic vs MorrisonsData class)
 /// </summary>
@@ -38,11 +41,34 @@ public class MorrisonsData
     public int SalesVolume { get; set; }
     [Name("R_SALES_VALUE")]
     public double SalesValue { get; set; }
+    //[Name("R_STORE_STOCK_VOLUME")]
+    //public int StoreStockVolume { get; set; }
+    //[Name("R_STORE_STOCKED")]
+    //public int StoreStocked { get; set; }
+    //[Name("R_STORE_RANGED")]
+    //public int StoreRanged { get; set; }
+
+    //[Name("A_RETAILER_NAME")]
+    //public string RetailerName { get; set; }
+    //[Name("R_STORE_CODE")]
+    //public string StoreCode { get; set; }
+    //[Name("R_STORE_NAME")]
+    //public string StoreName { get; set; }
+    //[Name("R_DATE")]
+    //public string Date { get; set; }
+    //[Name("R_SKU")]
+    //public string SKU { get; set; }
+    //[Name("S_PRODUCT_DESCRIPTION")]
+    //public string ProductDescription { get; set; }
+    //[Name("R_SALES_VOLUME")]
+    //public string SalesVolume { get; set; }
+    //[Name("R_SALES_VALUE")]
+    //public string SalesValue { get; set; }
     [Name("R_STORE_STOCK_VOLUME")]
-    public int StoreStockVolume { get; set; }
+    public string StoreStockVolume { get; set; }
     [Name("R_STORE_STOCKED")]
-    public int StoreStocked { get; set; }
+    public string StoreStocked { get; set; }
     [Name("R_STORE_RANGED")]
-    public int StoreRanged { get; set; }
+    public string StoreRanged { get; set; }
 }
 
